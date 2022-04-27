f39e5b668808cfd0d46cf27e30efeadb38ff53ab
Leonardo Lima <dev.leonardolima@gmail.com>
2022-04-27 15:42:19 -0400

    refactor the function (getData) that fetches the data

--- src/services/getData.js
+++ src/services/getData.js
@@ -1,39 +1,58 @@
 const api = require("../services/api");
 
-async function getCpuUsageData() {
+function getData() {
+  // 0: GetCpuUsageData
+  // 1: GetMemoryUsageData
+  // 2: GetClusterStatusInfo
+  const allData = [];
+
+  Promise.all([
+    api.get("b1bc5162-7cf2-4599-b1f5-e3bd58fcf07f"), // GetCpuUsageData
+    api.get("d23c3262-967e-4567-b7f6-2fd263748811"), // GetMemoryUsageData
+    api.get("cab2791c-7c85-4461-b95c-86bc1a12dc72"), // GetClusterStatusInfo
+  ]).then((response) => {
+    for (const res of response) {
+      const { data } = res;
+
+      allData.push(data);
+    }
+    console.log(allData);
+  });
+}
+
+module.exports = getData;
+
+/* 
+  https://run.mocky.io/v3/d23c3262-967e-4567-b7f6-2fd263748811 // GetCpuUsageData
+  https://run.mocky.io/v3/cab2791c-7c85-4461-b95c-86bc1a12dc72 // GetMemoryUsageData
+  https://run.mocky.io/v3/b1bc5162-7cf2-4599-b1f5-e3bd58fcf07f // GetClusterStatusInfo
+*/
+
+/* async function GetCpuUsageData() {
   const response = await api.get("b1bc5162-7cf2-4599-b1f5-e3bd58fcf07f");
 
   console.log(response.data);
 }
 
-async function getMemoryUsageData() {
+async function GetMemoryUsageData() {
   const response = await api.get("d23c3262-967e-4567-b7f6-2fd263748811");
 
   console.log(response.data);
 }
 
-async function getClusterStatusInfo() {
+async function GetClusterStatusInfo() {
   const response = await api.get("cab2791c-7c85-4461-b95c-86bc1a12dc72");
 
   console.log(response.data);
-}
+} */
 
-async function getData() {
-  console.time("resposta");
-  const cpuUsageData = await getCpuUsageData();
-  const memoryUsageData = await getMemoryUsageData();
-  const clusterStatusInfo = await getClusterStatusInfo();
-  console.timeEnd("resposta");
-  return {
-    cpu: cpuUsageData,
-    memory: memoryUsageData,
-    cluster: clusterStatusInfo,
-  };
-}
-
-console.log(getData());
 /* 
-  https://run.mocky.io/v3/d23c3262-967e-4567-b7f6-2fd263748811
-  https://run.mocky.io/v3/cab2791c-7c85-4461-b95c-86bc1a12dc72
-  https://run.mocky.io/v3/b1bc5162-7cf2-4599-b1f5-e3bd58fcf07f
-*/
+const cpuUsageData = await getCpuUsageData();
+const memoryUsageData = await getMemoryUsageData();
+const clusterStatusInfo = await getClusterStatusInfo();
+console.timeEnd("resposta");
+return {
+  cpu: cpuUsageData,
+  memory: memoryUsageData,
+  cluster: clusterStatusInfo,
+}; */
