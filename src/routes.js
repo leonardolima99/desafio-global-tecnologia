8b6b8b0b18bade7a76c70d4740dd09e1b91972ea
Leonardo Lima <dev.leonardolima@gmail.com>
2022-04-26 22:41:09 -0400

    create routes for user CRUD

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,21 @@
 const express = require("express");
 const routes = express.Router();
 
-routes.get("/", (req, res) => {
-  res.end("Hello Global Tecnologias!");
-});
+routes
+  .get("/", (req, res) => {
+    res.end("Hello Global Tecnologias!");
+  })
+  .get("/users", (req, res) => {
+    res.end("Rota para listar os usuários.");
+  })
+  .post("/users", (req, res) => {
+    res.end("Rota para criar um usuário.");
+  })
+  .put("/users/:id", (req, res) => {
+    res.end("Rota para atualizar um usuário.");
+  })
+  .delete("/users/:id", (req, res) => {
+    res.end("Rota para deletar um usuário.");
+  });
 
 module.exports = routes;
