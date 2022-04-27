14bc93e67e7355536fd5a288e4a7d1d4c7b525f9
Leonardo Lima <dev.leonardolima@gmail.com>
2022-04-27 07:08:34 -0400

    add file .env.example

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const routes = require("./routes");
 
 require("dotenv").config({ path: ".env" });
-console.log(process.env.NODE_ENV);
+
 const app = express();
 
 app.use(routes);
