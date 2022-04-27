6112df813dd3f71df8e1c774fc48627fa004c06a
Leonardo Lima <dev.leonardolima@gmail.com>
2022-04-27 15:58:18 -0400

    refactor: move getData() to controller.index

---
+++ src/controllers/elasticSearchController.js
@@ -0,0 +1,25 @@
+const api = require("../services/api");
+
+exports.index = async function (req, res) {
+  try {
+    // 0: GetCpuUsageData
+    // 1: GetMemoryUsageData
+    // 2: GetClusterStatusInfo
+    const allData = [];
+
+    const response = await Promise.all([
+      api.get("b1bc5162-7cf2-4599-b1f5-e3bd58fcf07f"), // GetCpuUsageData
+      api.get("d23c3262-967e-4567-b7f6-2fd263748811"), // GetMemoryUsageData
+      api.get("cab2791c-7c85-4461-b95c-86bc1a12dc72"), // GetClusterStatusInfo
+    ]);
+
+    for (const res of response) {
+      const { data } = res;
+
+      allData.push(data);
+    }
+    console.log(allData);
+  } catch (err) {
+    console.log(err.status, err.message);
+  }
+};
--- src/services/getData.js
+++ src/services/getData.js
@@ -1,23 +1,23 @@
 const api = require("../services/api");
 
-function getData() {
+async function getData() {
   // 0: GetCpuUsageData
   // 1: GetMemoryUsageData
   // 2: GetClusterStatusInfo
   const allData = [];
 
-  Promise.all([
+  const response = await Promise.all([
     api.get("b1bc5162-7cf2-4599-b1f5-e3bd58fcf07f"), // GetCpuUsageData
     api.get("d23c3262-967e-4567-b7f6-2fd263748811"), // GetMemoryUsageData
     api.get("cab2791c-7c85-4461-b95c-86bc1a12dc72"), // GetClusterStatusInfo
-  ]).then((response) => {
-    for (const res of response) {
-      const { data } = res;
+  ]);
 
-      allData.push(data);
-    }
-    console.log(allData);
-  });
+  for (const res of response) {
+    const { data } = res;
+
+    allData.push(data);
+  }
+  console.log(allData);
 }
 
 module.exports = getData;
