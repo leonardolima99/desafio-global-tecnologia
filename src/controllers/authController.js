1b50be4b5ea895663467af3618105174a542cb18
Leonardo Lima <dev.leonardolima@gmail.com>
2022-04-28 14:35:23 -0400

    remove password from return on sign in

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -11,16 +11,14 @@ exports.signin = async function (req, res) {
         email,
         senha,
       })
+      .select("email", "nivel_acesso")
       .first();
 
     if (!user) {
       return res.json({ message: "Email ou senha incorretos.", status: 401 });
     }
 
-    const token = await jwt.sign({
-      email: user.email,
-      nivel_acesso: user.nivel_acesso,
-    });
+    const token = jwt.sign(user);
     res.json({ auth: true, token });
   } catch (err) {
     console.log(`Sign in Error: ${err.status} -> ${err.message}`);
