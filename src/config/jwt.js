46502cada2a8570cb82bfb9771acdc041a4fbcef
Leonardo Lima <dev.leonardolima@gmail.com>
2022-04-27 22:17:22 -0400

    Revert "config jwt"
    This reverts commit 7b1a22ad173a9f2c094d6be6064416df386bc656.

--- src/config/jwt.js
+++
@@ -1,8 +0,0 @@
-const jwt = require("jsonwebtoken");
-
-const { SECRET } = process.env;
-
-module.exports = {
-  sign: (payload) => jwt.sign(payload, SECRET, { expiresIn: 86400 }),
-  verify: (token) => jwt.verify(token, SECRET),
-};
