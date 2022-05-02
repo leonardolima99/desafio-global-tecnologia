d9d17c4d141612e3926ea794b07c221531be2fbb
Leonardo Lima <dev.leonardolima@gmail.com>
2022-05-02 16:15:56 -0400

    add validation with express-validation

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const routes = express.Router();
 
+const { body, validationResult } = require("express-validator");
+
 const userController = require("./controllers/userController");
 const elasticSearchController = require("./controllers/elasticSearchController");
 const authController = require("./controllers/authController");
@@ -12,13 +14,55 @@ routes
     res.end("Desafio Global Tecnologia.");
   })
 
-  .post("/login", authController.signin)
+  .post(
+    "/login",
+    body("email").isEmail(),
+    body("senha").isLength({ min: 5 }),
+    (req, res, next) => {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+      }
+      next();
+    },
+    authController.signin
+  )
 
   .get("/helth-check", authWorker, elasticSearchController.index)
 
   .get("/users", authAdmin, userController.index)
-  .post("/users", authAdmin, userController.create)
-  .put("/users/:id", authAdmin, userController.update)
+  .post(
+    "/users",
+    authAdmin,
+    body("email").isEmail().withMessage("O e-mail é obrigatório."),
+    body("senha")
+      .isLength({ min: 5 })
+      .withMessage("A senha deve ter mais de 5 caracteres."),
+    (req, res, next) => {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+      }
+      next();
+    },
+    userController.create
+  )
+  .put(
+    "/users/:id",
+    authAdmin,
+    body("email").isEmail().withMessage("O e-mail é obrigatório."),
+    body("senha")
+      .isLength({ min: 5 })
+      .withMessage("A senha deve ser maior que 5."),
+    (req, res, next) => {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+      }
+      next();
+    },
+    userController.update
+  )
   .delete("/users/:id", authAdmin, userController.delete);
 
 module.exports = routes;
